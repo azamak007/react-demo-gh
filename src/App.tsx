b9119578e5ca5bfb7f26c7d06ab09febdaef4a97
azamak007 <noppawat.dev@outlook.com>
2025-09-27 16:01:34 +0700

    feat:(App) h1 test caching

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Boo Github Action Workshop ^_^</h1>
+      <h1>Boo Github Action Workshop ^_^ test caching</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
